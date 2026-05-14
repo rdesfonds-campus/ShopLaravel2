f36e710126e1e7e3d6d822bee8135ec9d6a00666
rdesfonds-campus <romain.desfonds@le-campus-numerique.fr>
2026-05-14 22:17:45 +0200

    P2 - Blade et Layouts

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,13 @@ class ProductController extends Controller
 {
     public function index()
     {
-        return 'Liste des produits';
+        $products = [
+            ['name' => 'T-shirt Laravel', 'price' => 29.90],
+            ['name' => 'Mug PHP', 'price' => 12.50],
+            ['name' => 'Sticker Composer', 'price' => 2.00],
+        ];
+
+        return view('products.index', compact('products'));
     }
 
     public function show($id)
